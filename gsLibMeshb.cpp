10ca71cdcf6234a8ca4aebdcebe6275a95ed4ce8
Alexis Gobé <alexis.gobe@inria.fr>
2025-10-08 11:21:27 +0200

    Implement LibMeshb writer for unstructured meshes
    Export gsMultiPatch and gsField to .meshb/.solb format.
    Includes boundary extraction and scalar/vector solution support.

--- gsLibMeshb.cpp
+++ gsLibMeshb.cpp
@@ -1,2 +1,24 @@
+/** @file gsLibMeshb.cpp
 
-#include <libmeshb7.h>
+    @brief Provides implementation of functions writing LibMeshb files.
+
+    This file is part of the G+Smo library.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+    Author(s): A. Gobe
+*/
+
+#include <gsCore/gsTemplateTools.h>
+#include <gsLibMeshb/gsLibMeshb.h>
+#include <gsLibMeshb/gsLibMeshb.hpp>
+
+namespace gismo
+{
+    // Explicit instantiations for common types
+    TEMPLATE_INST void gsWriteLibMeshb(const gsMultiPatch<real_t>&, std::string const&, unsigned, int);
+    TEMPLATE_INST void gsWriteLibMeshb(const gsField<real_t>&, std::string const&, unsigned, int);
+
+} // namespace gismo
